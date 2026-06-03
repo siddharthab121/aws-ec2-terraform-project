89b8b2387769a0d3fe1f2fb5a94852b73a8305bb
siddharthab121 <92613405+siddharthab121@users.noreply.github.com>
2026-06-03 17:29:00 +0530

    Add AWS security group and EC2 instance resources

---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "web_sg" {
+  name = "web-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "webserver" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [
+    aws_security_group.web_sg.id
+  ]
+
+  tags = {
+    Name = "GitHub-Terraform-Server"
+  }
+}
